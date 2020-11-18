9a1526c8d22bbb9048b2f95e689dbbca1e84756a
jamesacampbell <james@jamescampbell.us>
2020-11-18 17:21:07 -0500

    Improving the fix

--- scripts/convertToParquet.py
+++ scripts/convertToParquet.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from pandas.io.json import json_normalize
 
 import os
 chd = os.chdir(f"{os.environ['HOME']}/projects/aws2open/")
+
+mydf = pd.read_json("json/keys.json", orient='records', dtype='dict')
+table_from_pandas = pa.Table.from_pandas(mydf)
+pq.write_table(table_from_pandas, 'parquet/keys.parquet')
