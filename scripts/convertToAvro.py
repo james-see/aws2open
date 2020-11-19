2c6f120e1b98347ed8b1eea2e2cb94ad8258ee3b
jamesacampbell <james@jamescampbell.us>
2020-11-19 00:24:18 -0500

    this doesn't really make things faster, but I tried

--- scripts/convertToAvro.py
+++ scripts/convertToAvro.py
@@ -6,28 +6,32 @@ import pandas as pd
 import pandavro as pdx
 
 chd = os.chdir(f"{os.environ['HOME']}/projects/aws2open/")
-OUTPUT_PATH=f"{chd}keys.avro"
+OUTPUT_PATH=f"{os.environ['HOME']}/projects/aws2open/avro/keys.avro"
 
 def main():
     cooljson = list()
     with open('json/keys.json') as f:
         d = json.load(f)
     # print(d)
-    for k,v in d.items():
-        if isinstance(v, dict):
-            print(v)
-            for nestedk, nestedv in v.items():
-                if isinstance(nestedv, list):
-                    for item in nestedv:
-                        print(item)
-                        cooljson.append({k: str(item)})
+    for k in d:
+        if isinstance(k, dict):
+            # print(k)
+            for somek, somev in k.items():
+                if isinstance(somev, dict):
+                    for ik, iv in somev.items():
+                        if isinstance(iv, list):
+                            for listitem in iv:
+                                for nestedk, nestedv in listitem.items():
+                                    print(nestedv)
+                                    cooljson.append({ik: str(nestedv)})
     print(cooljson)
-    mydf = pd.json_normalize(cooljson)
-    print(mydf.head(3))
+    mydf = pd.DataFrame.from_records(cooljson)
+    print(mydf.head(10))
+    mydffixed = mydf.replace(np.nan, '', regex=True)
     # mydf = pd.read_json("json/keys.json", orient='records', dtype='dict')
-    # pdx.to_avro(OUTPUT_PATH, mydf)
-    # saved = pdx.read_avro(OUTPUT_PATH)
-    # print(saved)
+    pdx.to_avro(OUTPUT_PATH, mydffixed)
+    saved = pdx.read_avro(OUTPUT_PATH)
+    print(saved)
 
 
 if __name__ == '__main__':
